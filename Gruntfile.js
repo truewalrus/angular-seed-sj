95487e68ed447757d61186d28dbb6bb682b22f0b
your name <rekursiv@gmail.com>
2013-05-21 12:03:45 -0700

    Began clean-up of directory structure/files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,95 +1,110 @@
 module.exports = function(grunt) {
-'use strict';
+	'use strict';
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    /*
-		uglify minifies and concatenates all angular.js files into app/production/build.js for production
-		mangle is set to false in the options because this disrupted the angular source files and caused errors
-	*/
-    uglify: {
-		options: {
-			banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-		},
-		build: {
-			src: [ 'app/js/**/*.js' ],
-			dest: 'app/production/build.js'
-		},
-		server: {
-			src: [ 'server.js' ],
-			dest: 'server.min.js'
-		}
-    },
-    //converts less to css in app/production/css/base.css
-	less: {
-		base: {
-			src: ['app/less/base/*.less', 'app/less/third-party/*.less'],
-			dest: 'app/production/css/base.css',
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		
+		// Uglify minifies and concatenates all js files for release.
+		uglify: {
 			options: {
-				yuicompress: true
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */'
+			},
+			release: {
+				files: {
+					'release/server.min.js': [ 'server.js' ],
+					'release/app/js/app.min.js': [ 'app/js/**/*.js' ]
+				}
 			}
-		}
-    },
-    //runs all angularjs files through jshint for linting purposes
-	jshint: {
-		all: ['Gruntfile.js', 
-				'app/js/*.js', 
-				'app/js/directives/*.js',
-				'app/js/services/*.js',
-				'app/js/filters/*.js', 
-				'app/js/controllers/*.js'
+		},
+		//converts less to css in app/production/css/base.css
+		less: {
+			base: {
+				src: [
+					'app/less/base/*.less',
+					'app/less/third-party/*.less'
+				],
+				dest: 'app/production/css/base.css',
+				options: {
+					yuicompress: true
+				}
+			},
+			release: {
+				options: {
+					yuicompress: true
+				},
+				files: {
+					'release/app/css/base.min.css': [
+						'app/less/base/*.less',
+						'app/less/third-part/*.less'
+					]
+				}
+			}
+		},
+		//runs all angularjs files through jshint for linting purposes
+		jshint: {
+			all: [
+				'Gruntfile.js',
+				'app/js/**/*.js'
 			],
-        //options set to make angular agree with jshint a bit more
-		options:{
-			globalstrict: true,
-			jquery: true,
-			node: true,
-			sub: true,
-			/*
+			//options set to make angular agree with jshint a bit more
+			options:{
+				globalstrict: true,
+				jquery: true,
+				node: true,
+				sub: true,
+				/*
 				sets some global variable names that jshint will not throw errors for
 				without this, it will complain that these global variables are not defined and such
-			*/
-			globals: { "angular": false, "FB": false, "window": false, "document":false, "navigator": false}
-		}
-    },
-    //not currently set up, karma is a testing plugin
-	karma: {
-		unit: {
-			configFile: 'config/karma.conf.js',
-			autoWatch: true
-		}
-	},
-    /*
+				*/
+				globals: {
+					"angular":		false,
+					"FB":			false,
+					"window":		false,
+					"document":		false,
+					"navigator":	false
+				}
+			}
+		},
+		//not currently set up, karma is a testing plugin
+		karma: {
+			unit: {
+				configFile: 'config/karma.conf.js',
+				autoWatch: true
+			}
+		},
+		/*
 		concatenates all third party js files in app/lib/third-party-js inclusions into one file, app/production/lib/js/third-party.js 
 		this allows for easier script inclusion in index.html
-	*/
-	concat: {
-		dist: {
-			src: ['app/lib/third-party-js/*.js'],
-			dest: 'app/production/lib/js/third-party.js'
+		*/
+		concat: {
+			third_party: {
+				src: ['app/lib/third-party-js/*.js'],
+				dest: 'app/production/lib/js/third-party.js'
+			}
 		}
-  }
-  });
-  
-  
+	});
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  
-  //Load the plugin that provides the "less" task.
-  grunt.loadNpmTasks('grunt-contrib-less');
-  
-  //Load the plugin that provides the "jsHint" task.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  
-  //Load the plugin that provides the "karma" task.
-  grunt.loadNpmTasks('grunt-karma');
-  
-  //Load the plugin that provides the "concatenate" task.
-  grunt.loadNpmTasks('grunt-contrib-concat');
 
-  // Default task(s). Add task here if you want it to run every time grunt is run
-  grunt.registerTask('default', ['uglify', 'less', 'jshint', 'concat']);
+
+	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	//Load the plugin that provides the "less" task.
+	grunt.loadNpmTasks('grunt-contrib-less');
+
+	//Load the plugin that provides the "jsHint" task.
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+
+	//Load the plugin that provides the "karma" task.
+	grunt.loadNpmTasks('grunt-karma');
+
+	//Load the plugin that provides the "concatenate" task.
+	grunt.loadNpmTasks('grunt-contrib-concat');
+
+	// Default task(s). Add task here if you want it to run every time grunt is run
+	grunt.registerTask('default', ['less:base', 'jshint', 'concat']);
+	
+	grunt.registerTask('release', ['uglify:release', 'less:release']);
 
 };
